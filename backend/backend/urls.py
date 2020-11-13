6ecfb176d9a80343214853b85657df755bf2ba6e
zunaedsifat <zunaed.sifat@gmail.com>
2020-11-14 00:41:23 +0600

    added test api endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import response, status, decorators
+
+
+@decorators.api_view(['GET'])
+def test_endpoint(request):
+    return response.Response({'details': 'working'}, status=status.HTTP_200_OK)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework_social_oauth2.urls')),
+    path('test/', test_endpoint),
 ]
